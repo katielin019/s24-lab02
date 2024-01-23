fdbc330ad42b31b288da6a1b203e8817785fbb31
Katie Lin <katielin019@gmail.com>
2024-01-23 16:37:16 -0500

    added getArea() method to Shape interface; annotated implementations of getArea with @Override

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -1,5 +1,10 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public interface Shape {
-    
+    /**
+     * Calculates and returns the area of the shape.
+     * 
+     * @return
+     */
+    double getArea();
 }
