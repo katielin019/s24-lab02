d098c61dd063b69b9d2f5f29ff8bd5583981ff27
Katie Lin <katielin019@gmail.com>
2024-01-24 10:05:53 -0500

    incr Square encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -3,14 +3,18 @@ package edu.cmu.cs.cs214.lab02.shapes;
 import edu.cmu.cs.cs214.lab02.Shape;
 
 public class Square implements Shape {
-    public double sideLen;
+    private double sideLen;
 
     public Square(double sideLen) {
         this.sideLen = sideLen;
     }
+
+    public double getSideLen() { return this.sideLen; }
+
+    public void setSideLen(double sideLen) { this.sideLen = sideLen; }
     
     @Override
     public double getArea() {
-        return sideLen * sideLen;
+        return this.sideLen * this.sideLen;
     }
 }
