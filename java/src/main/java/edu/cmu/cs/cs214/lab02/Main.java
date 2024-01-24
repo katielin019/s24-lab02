9499e660ac251920798dad93dcfebe0705bc43d5
Katie Lin <katielin019@gmail.com>
2024-01-24 10:11:08 -0500

    added code to main to test Renderer w/ various shapes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        rectangleRenderer.draw();
 
-        Renderer renderer = new Renderer(rectangle);
+        Shape circle = new Circle(2);
+        Renderer circleRenderer = new Renderer(circle);
+        circleRenderer.draw();
 
-        renderer.draw();
+        Shape square = new Square(4);
+        Renderer squareRenderer = new Renderer(square);
+        squareRenderer.draw();
     }
 }
